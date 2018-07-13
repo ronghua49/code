77fb2d7220f92fe9fdc859e5f547d3ce6869e8af
ronghua49 <1540307734@qq.com>
2018-07-13 22:40:33 +0800

    sping的搭建和Bean管理的配置

---
+++ spring/src/test/java/ApplicationContextBeanTest.java
@@ -0,0 +1,16 @@
+import com.haohua.dao.UserDao;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextBeanTest {
+
+@Test
+    public  void find(){
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    UserDao userDao = (UserDao) context.getBean("userDao");
+        userDao.insert();
+    }
+
+
+}
