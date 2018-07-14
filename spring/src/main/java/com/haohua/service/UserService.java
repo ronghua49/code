c09a20a2d2bed2b0e5252b71891f636173a8f217
ronghua49 <1540307734@qq.com>
2018-07-14 14:55:06 +0800

    IOC DI 类依赖注入的练习

---
+++ spring/src/main/java/com/haohua/service/UserService.java
@@ -0,0 +1,86 @@
+package com.haohua.service;    /*
+ * @author  Administrator
+ * @date 2018/7/14
+ */
+
+import com.haohua.dao.UserDao;
+import com.haohua.pojo.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class UserService {
+
+    private UserDao userDao;
+    private User user;
+    private Integer num;
+    private List<String> list;
+    private Map<String ,Object> map;
+    private Set<Double> set;
+    private Properties properties;
+
+    //提供对象的set方法，让容器把生成的对象注入到属性
+    public void setUserDao(UserDao userDao){
+        this.userDao = userDao;
+    }
+    public void setUser(User user){
+        this.user = user;
+    }
+    public UserService(){}
+    //提供类的构造方法，在此类创建时，依赖spring容器给予的对象，一同创建
+    public UserService(UserDao userDao){
+        this.userDao=userDao;
+    }
+    public UserService(User user){
+        this.user = user;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public Set<Double> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<Double> set) {
+        this.set = set;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public void fu(){
+        userDao.insert();
+    }
+    public void fun2(){
+        System.out.println(user);
+    }
+}
---
+++ spring/src/test/java/ServiceInjectTest.java
@@ -0,0 +1,41 @@
+import com.haohua.service.UserService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.*;
+
+public class ServiceInjectTest {
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    @Test
+    public void daoInjectTest(){
+    UserService userService = (UserService) context.getBean("userService2");
+        userService.fun2();
+    }
+    @Test
+    public void propertiesInjectTest(){
+        UserService userService3 = (UserService) context.getBean("userService3");
+        System.out.println(userService3.getNum());
+       List<String> list = userService3.getList();
+        for (String string: list) {
+            System.out.println(string);
+        }
+        Map<String,Object> map = userService3.getMap();
+        for(Map.Entry<String,Object> entry : map.entrySet()){
+            System.out.println(entry.getKey()+"-->"+entry.getValue());
+        }
+        Set<Double> set = userService3.getSet();
+        for (Double doubleSet:set){
+            System.out.println(doubleSet);
+        }
+        Properties properties = userService3.getProperties();
+        Enumeration<Object> enumerations = properties.keys();
+        while(enumerations.hasMoreElements()){
+            Object key = enumerations.nextElement();
+            Object value= properties.get(key);
+            System.out.println(key+"-->"+value);
+        }
+    }
+}
