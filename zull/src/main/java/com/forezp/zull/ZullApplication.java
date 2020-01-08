ee7bd5efe4f2d059847321fdab0810be0f033a94
ronghua49 <1540307734@qq.com>
2020-01-08 08:54:57 +0800

    zull 网关的基本使用

---
+++ zull/src/main/java/com/forezp/zull/ZullApplication.java
@@ -0,0 +1,15 @@
+package com.forezp.zull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZullApplication.class, args);
+    }
+
+}
---
+++ zull/src/test/java/com/forezp/zull/ZullApplicationTests.java
@@ -0,0 +1,12 @@
+package com.forezp.zull;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
